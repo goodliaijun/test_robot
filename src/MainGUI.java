b897077e399fa62f5e9f81a4a3727d2de9b38eb9
goodliaijun <goodliaijun@126.com>
2016-05-01 16:43:11 +0800

    add a class robot_sleep

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -5,6 +5,7 @@ public class MainGUI {
 		// TODO Auto-generated method stub
 		System.out.println("hello, I am HappyBoy!");
 		System.out.println("hello, how are you!");
+		robot_sleep.robot_go_to_sleep();
 
 	}
 
---
+++ src/robot_sleep.java
@@ -0,0 +1,6 @@
+
+public class robot_sleep {
+	
+	public  static void robot_go_to_sleep(){
+	System.out.println("I go to bed , good night!");}
+}
