e3ed357948b7ad418bcafeed6f8b1f049d2344ac
goodliaijun <aijunlee@qq.com>
2016-05-01 16:26:24 +0800

    good luck

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -4,6 +4,7 @@ public class MainGUI {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello, I am HappyBoy!");
+		System.out.println("hello, how are you!");
 
 	}
 
